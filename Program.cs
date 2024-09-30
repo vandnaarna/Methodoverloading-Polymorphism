f1b921005928291bad515bf77c902158ee224bfe
Vandana chouhan <vandanachouhan@arnasoftech.com>
2024-09-30 16:17:45 +0530

    messegesFetch

--- Program.cs
+++ Program.cs
@@ -15,7 +15,12 @@ namespace Methodoverloading
         {
             public void multiply(int a, int b)
             {
-               
+
+
+                Console.WriteLine("Multiplication of a and b is:");
+                Console.WriteLine("Multiplication of a and b is:");
+                Console.WriteLine("Multiplication of a and b is:");
+                Console.WriteLine("Multiplication of a and b is:");
                 Console.WriteLine("Multiplication of a and b is:");
                 Console.WriteLine(a * b);
             }
